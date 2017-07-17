b4356daa4a9c9ffa348723b6b2af39fe044c6758
Khanagate <baneofdarkness@hotmail.com>
2017-07-17 15:40:25 +1000

    Display Player Cards
    Still working on fixing the gap between the cards

--- Assets/Scripts/SingleplayerManager.cs
+++ Assets/Scripts/SingleplayerManager.cs
@@ -8,44 +8,70 @@ public class SingleplayerManager : MonoBehaviour {
 	public BallManager BM_Script;
 
 	public int numPlayers;
-	public List<string> Cards;
+	public List<string> cards;
+    public GameObject cardPrefab;
+    public List<GameObject> playerCards;
+    public float cardWidth;
 
 	// Use this for initialization
 	void Awake () {
 		GM_Script = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GM>();
 		BM_Script = GameObject.FindGameObjectWithTag("BallManager").GetComponent<BallManager>();
 
-		// Cards from actual bingo to test
-		//Card 1:12,4,8,6,7,27,25,24,26,17,35,34,0,43,32,56,52,59,54,53,61,63,65,74,62,
-		//Card 2:6,15,7,13,8,20,25,16,17,26,45,36,0,35,33,56,51,58,52,49,63,71,69,74,68,
-		//Card 3:11,5,2,3,12,16,21,26,30,18,34,35,0,32,43,57,54,56,55,46,73,64,66,72,63,
-		//Card 4:10,15,7,13,8,20,25,16,17,26,45,36,0,35,33,56,51,58,52,49,63,71,69,74,68
-		//Card 5:14,5,3,11,1,29,18,19,23,21,36,39,0,37,41,48,58,50,55,57,69,72,64,68,70,
+        // Cards from actual bingo to test
+        //Card 1:12,4,8,6,7,27,25,24,26,17,35,34,0,43,32,56,52,59,54,53,61,63,65,74,62,
+        //Card 2:6,15,7,13,8,20,25,16,17,26,45,36,0,35,33,56,51,58,52,49,63,71,69,74,68,
+        //Card 3:11,5,2,3,12,16,21,26,30,18,34,35,0,32,43,57,54,56,55,46,73,64,66,72,63,
+        //Card 4:10,15,7,13,8,20,25,16,17,26,45,36,0,35,33,56,51,58,52,49,63,71,69,74,68
+        //Card 5:14,5,3,11,1,29,18,19,23,21,36,39,0,37,41,48,58,50,55,57,69,72,64,68,70,
+        //cardWidth = 1572;
+        cardWidth = cardPrefab.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        numPlayers = 5;
 
-		numPlayers = 3;
-	}
+        //cardPrefab.GetComponentInChildren<BingoBlueButton>().enabled = false;
+        /* Component[] scripts = cardPrefab.GetComponentsInChildren<BingoBlueButton>();
+
+        foreach (BingoBlueButton script in scripts)
+        {
+            script.enabled = false;
+        } */
+
+        //cardPrefab.
+        //cardPrefab.Get
+
+        //playercard.GetComponent<Vector3>().
+
+    }
 	/*
 	// Update is called once per frame
 	void Update () {
 		
 	}
 	*/
+
 	public void initialiseGame()
 	{
-		Cards.Clear();
+        cards.Clear();
 		for (int i = 1; i < numPlayers + 1; i++)
 		{
-			Cards.Add(generateCard(i));
+            cards.Add(generateCard(i));
 		}
 
-		/// Display cards under here?
-		/// Could duplicate bingo cards, use ballsprite numbers to represent card numbers for now
-		/// Just need to zoom out camera if possible
-		/// 
-		/// bingoCardMiniPrefabs = Instantiate(Resources.Load("Prefabs/BingoCardMini"), new Vector3(8.8f, -6.77f, 0), Quaternion.identity) as GameObject;
-		/// BingoCardBigScript = GameObject.FindGameObjectWithTag ("BingoCardBig").GetComponent<BingoCardBig> ();
-		/// public BingoCardBig BingoCardBigScript;
-	}
+        displayCards();
+
+        /// Display cards under here?
+        /// Could duplicate bingo cards, use ballsprite numbers to represent card numbers for now
+        /// Just need to zoom out camera if possible
+        /// 
+        /// bingoCardMiniPrefabs = Instantiate(Resources.Load("Prefabs/BingoCardMini"), new Vector3(8.8f, -6.77f, 0), Quaternion.identity) as GameObject;
+        /// BingoCardBigScript = GameObject.FindGameObjectWithTag ("BingoCardBig").GetComponent<BingoCardBig> ();
+        /// public BingoCardBig BingoCardBigScript;
+    }
+
+    public void showPlayerCards()
+    {
+
+    }
 
 	private string generateCard(int player)
 	{
@@ -88,6 +114,25 @@ public class SingleplayerManager : MonoBehaviour {
 		Debug.Log(tempCard);
 		return tempCard;
 	}
+
+    public void displayCards()
+    {
+        //GameObject tempCard;
+        
+        //tempCard.transform.parent = this;
+        for (int i = 0; i < numPlayers+0; i++)
+        {
+            //tempCard = Instantiate(cardPrefab, new Vector3(-20f + 1f + ((cardWidth * 0.4f) * i), this.transform.position.y, this.transform.position.z), Quaternion.identity);
+            //tempCard.transform.parent = GameObject.Find("Singleplayer").transform;
+            //cardPrefab.GetComponent<GameObject>().transform.position.x
+            // tempCard = Instantiate(cardPrefab, new Vector3(cardPrefab.GetComponent<GameObject>().transform.position.x + 1f + ((cardWidth * 0.4f) * i), cardPrefab.GetComponent<GameObject>().transform.position.y, cardPrefab.GetComponent<GameObject>().transform.position.z), Quaternion.identity);
+            //cardPrefab.GetComponent<GameObject>().transform.position.x
+            playerCards.Add(Instantiate(cardPrefab, new Vector3(-20f + ((cardWidth * 0.4f) * i), this.transform.position.y, this.transform.position.z), Quaternion.identity) as GameObject);
+
+            //cardPrefab.GetComponent<>
+        }
+
+    }
 
 	private bool numberExists(Dictionary<int,int> dictionary, int number)
 	{
